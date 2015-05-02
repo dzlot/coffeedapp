f39873d1622a7cf9701e995a16d352fa78e17e7c
David Zlotnick <David@Davids-iMac.local>
2015-05-02 09:31:07 -0400

    Changes to the settings file!

--- coffeedapp/settings.py
+++ coffeedapp/settings.py
@@ -87,14 +87,14 @@ if ON_HEROKU == True:
 
     DATABASES['default'] = dj_database_url.config()
     DATABASES['default']['ENGINE'] = 'django-postgrespool'
-else: 
+# else: 
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
-        }
-    }
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(MAIN_DIR, 'db.sqlite3'),
+#         }
+#     }
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
