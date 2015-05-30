5f72c51cff01ab574b025049c114bf3583366dd5
David Zlotnick <David@Davids-iMac.local>
2015-05-30 08:50:51 -0400

    A bunch of new changes, added forms and sign-up pages!

--- core/models.py
+++ core/models.py
@@ -16,6 +16,32 @@ RATING_CHOICES = (
 	(5, '*****'),
 	)
 
+YESNO_CHOICES = (
+	(0, 'No'),
+	(1, 'Yes'),
+	)
+
+PLURAL_CHOICES = (
+	(0, 'None'),
+	(1, 'Minimal'),
+	(2, 'Some'),
+	(3, 'Ample'),
+	)
+
+WIFI_CHOICES = (
+	(0, 'None'),
+	(1, 'Spotty'),
+	(2, 'Strong'),
+	)
+
+COFFEE_CHOICES = (
+	(0, 'None'),
+	(1, 'Truck Stop'),
+	(2, 'Good'),
+	(3, 'Really Good'),
+	(4, 'Great'),
+	)
+
 def upload_to_location(instance, filename):
     blocks = filename.split('.')
     ext = blocks[-1]
@@ -32,6 +58,14 @@ class Location(models.Model):
 	hours = models.TextField(null=True, blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 	image_file = models.ImageField(upload_to=upload_to_location, null=True, blank=True)
+	wifi = models.IntegerField(choices=WIFI_CHOICES, null=True, blank=True)
+	seating = models.IntegerField(choices=PLURAL_CHOICES, null=True, blank=True)
+	outlets = models.IntegerField(choices=PLURAL_CHOICES, null=True, blank=True)
+	bathrooms = models.IntegerField(choices=YESNO_CHOICES, null=True, blank=True)
+	coffee = models.IntegerField(choices=COFFEE_CHOICES, null=True, blank=True)
+	alcohol = models.IntegerField(choices=YESNO_CHOICES, null=True, blank=True)
+	outdoor = models.IntegerField(choices=YESNO_CHOICES, null=True, blank=True)
+	food = models.IntegerField(choices=YESNO_CHOICES, null=True, blank=True)
 
 	def get_absolute_url(self):
 		return reverse(viewname="location_list", args=[self.id])
@@ -45,6 +79,9 @@ class Location(models.Model):
 			return average
 		else:
 			return int(average)
+			
+	def get_reviews(self):
+		return self.review_set.all()
 
 class Review(models.Model):
 	location = models.ForeignKey(Location)
